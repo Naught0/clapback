bb2fd8700b54142aac14cd9492a6b5d649a2e60a
James <4589250+Naught0@users.noreply.github.com>
2020-03-04 17:07:58 -0500

    initial commit

---
+++ js/clap.js
@@ -0,0 +1,42 @@
+$(function () {
+    var isDarkMode = false;
+
+    $('#user-in').keyup(function () {
+        $('#user-out').val(this.value.split(' ').join(' 👏 '));
+    })
+
+    $('#btn-clear').click(function() {
+        $('#user-in').val('');
+        $('#user-out').val('');
+    })
+    
+    $('#btn-darkmode').click(function(){
+        if (isDarkMode){
+            document.styleSheets[3].disabled = false;
+            this.innerHTML = "Dark Mode"
+            isDarkMode = false
+        }
+        else{
+            this.innerHTML = "Light Mode"
+            document.styleSheets[3].disabled = true;
+            isDarkMode = true
+        }
+    })
+
+    var clip = new ClipboardJS('#btn-copy');
+    clip.on('success', function (e) {
+        $('#user-out').notify("Copied!", {
+            position: "right",
+            className: "success"
+        });
+        console.info('Action:', e.action);
+        console.info('Trigger:', e.trigger);
+
+        e.clearSelection();
+    });
+
+    clip.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
+})
