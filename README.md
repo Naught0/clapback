7af8df76bcf54ed4e9dd93690f78756a24014973
James <>
2020-03-05 22:30:37 -0500

    finish sponge
    TODO:
    imgflip api to generate spongebob meme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# clapback
+# [clapback]('https://naught0.github.io/clapback')
  a stupid website for people fond of clapping
+
+## Now introducing: sPOnGeBob !
+ mock your friends because you have nothing intelligent left to contribute 
--- js/clap.js
+++ js/clap.js
@@ -1,21 +1,19 @@
 $(function () {
     /* Dark mode stuff */
-    if (localStorage.getItem('darkmode') == 'true'){
+    if (localStorage.getItem('darkmode') == 'true') {
         document.styleSheets[4].disabled = true;
         var isDarkMode = true;
-    }
-    else {
+    } else {
         var isDarkMode = false;
     }
 
-    $('#btn-darkmode').click(function(){
-        if (isDarkMode){
+    $('#btn-darkmode').click(function () {
+        if (isDarkMode) {
             document.styleSheets[4].disabled = false;
             this.innerHTML = "Dark Mode"
             localStorage.setItem('darkmode', 'false')
             isDarkMode = false
-        }
-        else{
+        } else {
             this.innerHTML = "Light Mode"
             document.styleSheets[4].disabled = true;
             localStorage.setItem('darkmode', 'true')
@@ -28,34 +26,46 @@ $(function () {
         $('#user-out').val(this.value.split(' ').join(' 👏 '));
     })
 
-    $('#btn-clear').click(function() {
+    $('#btn-clear').click(function () {
         $('#user-in').val('');
         $('#user-out').val('');
     })
 
+    function jumble(){
+        $('#sponge-out').val(document.getElementById('sponge-in').value.split('').map((v)=>
+            Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()
+            ).join(''))
+    }
     /* Sponge stuff */
-    $('#sponge-in').keyup(function() {
-        var newstr = "";
-        for (var c of this.value){
-            if (Math.floor(Math.random() * 101) > 50){
-                newstr += c.toUpperCase();
-            }
-            else{
-                newstr += c;
-            }
-        }
-        $('#sponge-out').val(newstr)
+    $('#sponge-in').keyup(delay(function (e) {
+        // $('#sponge-out').val(this.value.split('').map((v) =>
+        //     Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()
+        // ).join(''))
+        jumble();
+    }, 350));
+
+    $('#sponge-jumble').click(function() {
+        jumble();
+    })
+
+    $('.btn-copy').click(function () {
+        $(this).notify("Copied!", {
+            className: "success",
+            showAnimation: "fadeIn",
+            hideAnimation: "fadeOut",
+            gap: 5,
+            position: "top-middle"
+        })
+    })
+
+    $('#sponge-clear').click(function(){ 
+        $('#sponge-in').val('');
+        $('#sponge-out').val('');
     })
 
     /* Clipboard JS stuff */
     var clip = new ClipboardJS('.btn-copy');
     clip.on('success', function (e) {
-        $.notify("Copied!", {
-            position: "top-right",
-            className: "success",
-            showAnimation: "fadeIn",
-            hideAnimation: "fadeOut"
-        });
         console.info('Action:', e.action);
         console.info('Trigger:', e.trigger);
 
