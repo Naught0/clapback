07fe206249e039d3d7755c90c99513bbe9713e63
James <4589250+Naught0@users.noreply.github.com>
2020-03-06 13:57:52 -0500

    WILL THIS WORK? WHO KNOWS

--- js/clap.js
+++ js/clap.js
@@ -1,4 +1,7 @@
 $(function () {
+    /* imgflip spongebob meme id */
+    const spongeID = 102156234;
+    const memeURL = "http://api.imgflip.com/caption_image";
     /* Dark mode stuff */
     if (localStorage.getItem('darkmode') == 'true') {
         document.styleSheets[4].disabled = true;
@@ -6,6 +9,20 @@ $(function () {
     } else {
         var isDarkMode = false;
     }
+
+    /* Clipboard JS stuff */
+    var clip = new ClipboardJS('.btn-copy');
+    clip.on('success', function (e) {
+        console.info('Action:', e.action);
+        console.info('Trigger:', e.trigger);
+
+        e.clearSelection();
+    });
+
+    clip.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
 
     $('#btn-darkmode').click(function () {
         if (isDarkMode) {
@@ -31,10 +48,10 @@ $(function () {
         $('#user-out').val('');
     })
 
-    function jumble(){
-        $('#sponge-out').val(document.getElementById('sponge-in').value.split('').map((v)=>
+    function jumble() {
+        $('#sponge-out').val(document.getElementById('sponge-in').value.split('').map((v) =>
             Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()
-            ).join(''))
+        ).join(''))
     }
     /* Sponge stuff */
     $('#sponge-in').keyup(delay(function (e) {
@@ -44,7 +61,7 @@ $(function () {
         jumble();
     }, 350));
 
-    $('#sponge-jumble').click(function() {
+    $('#sponge-jumble').click(function () {
         jumble();
     })
 
@@ -58,22 +75,33 @@ $(function () {
         })
     })
 
-    $('#sponge-clear').click(function(){ 
+    $('#sponge-clear').click(function () {
         $('#sponge-in').val('');
         $('#sponge-out').val('');
     })
 
-    /* Clipboard JS stuff */
-    var clip = new ClipboardJS('.btn-copy');
-    clip.on('success', function (e) {
-        console.info('Action:', e.action);
-        console.info('Trigger:', e.trigger);
-
-        e.clearSelection();
-    });
-
-    clip.on('error', function (e) {
-        console.error('Action:', e.action);
-        console.error('Trigger:', e.trigger);
-    });
+    /* Memeify */
+    $('#sponge-memeify').click(function () {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        
+        var formdata = new FormData();
+        formdata.append("template_id", 102156234);
+        formdata.append("username", "naught0");
+        formdata.append("password", "D2UsVjf#D1JJ");
+        formdata.append("text0", $('#sponge-out').value);
+        
+        var requestOptions = {
+          method: 'POST',
+          
+          headers: myHeaders,
+          body: formdata,
+          redirect: 'follow'
+        };
+        
+        fetch("https://api.imgflip.com/caption_image?template_id=&password=D2UsVjf#D1JJ#D1JJ", requestOptions)
+          .then(response => response.text())
+          .then(result => $('#sponge-meme-link').attr('href', result))
+          .catch(error => console.log('error', error));
+    })
 })
